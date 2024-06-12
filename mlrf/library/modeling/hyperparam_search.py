dd23bd76cd2d1a8d5223469dc3960f0fc5648a64
Paul Giraud <paul.giraud@epita.fr>
2024-06-12 14:51:45 +0200

    nb update

--- mlrf/library/modeling/hyperparam_search.py
+++ mlrf/library/modeling/hyperparam_search.py
@@ -4,12 +4,13 @@ import os
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 class HyperparamsSearch:
-    def __init__(self, hyperparams, model, verbose=0, cv=None, n_jobs=None):
+    def __init__(self, hyperparams, model, verbose=0, cv=None, n_jobs=None,niters=10):
         self.hyperparams = hyperparams
         self.model = model
         self.verbose = verbose
         self.cv = cv
         self.n_jobs = n_jobs
+        self.niters = niters
         
     def set_verbose_level(self, verbose):
         self.verbose = verbose
@@ -20,6 +21,9 @@ class HyperparamsSearch:
     def set_njobs(self, n_jobs):
         self.n_jobs = n_jobs
         
+    def set_niters(self, niters):
+        self.niters = niters
+        
     def grid_search(self, X_train, y_train):
         gs = GridSearchCV(estimator=self.model, param_grid=self.hyperparams,
                                    cv=self.cv, n_jobs=self.n_jobs, verbose=self.verbose)
@@ -27,9 +31,9 @@ class HyperparamsSearch:
         
         return gs.best_estimator_, gs.best_params_
     
-    def random_search(self):
-        rs = RandomizedSearchCV(estimator=self.model, param_grid=self.hyperparams,
-                                   cv=self.cv, n_jobs=self.n_jobs, verbose=self.verbose)
+    def random_search(self, X_train, y_train):
+        rs = RandomizedSearchCV(estimator=self.model, param_distributions=self.hyperparams,
+                                   cv=self.cv, n_jobs=self.n_jobs, verbose=self.verbose,n_iter=self.niters)
         rs.fit(X_train, y_train)
         
         return rs.best_estimator_, rs.best_params_
