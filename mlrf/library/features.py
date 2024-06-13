416b1a190a3ef737ac6f3f6494622f7fa854dcf2
Fabien Holard <fabien.holard@epita.fr>
2024-06-13 11:06:31 +0200

    fix: sift

--- mlrf/library/features.py
+++ mlrf/library/features.py
@@ -3,40 +3,6 @@ import numpy as np
 from skimage.feature import hog
 from skimage import color
 
-# Assurez-vous que les images sont en format 3D (nombre d'images, hauteur, largeur, canaux)
-# Par exemple, si vos images sont en format (nombre d'images, hauteur * largeur * canaux), 
-# vous pouvez les reshaper:
-# num_images = images.shape[0]
-# images = images.reshape(num_images, 32, 32, 3)  # Supposons des images 32x32x3
-
-def extract_features(images):
-    features = []
-    for img in images:
-        # Convertir en niveaux de gris
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        
-        # Redimensionner l'image (par exemple à 32x32 si nécessaire)
-        gray = cv2.resize(gray, (32, 32))
-        
-        # Extraire les histogrammes de couleurs (3 canaux)
-        hist = cv2.calcHist([img], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256]).flatten()
-        
-        # Extraire les descripteurs de texture (LBP)
-        lbp = local_binary_pattern(gray, 8, 1, method="uniform")
-        (hist_lbp, _) = np.histogram(lbp.ravel(), bins=np.arange(0, 10), range=(0, 9))
-        
-        # Normaliser les histogrammes
-        hist_lbp = hist_lbp.astype("float")
-        hist_lbp /= (hist_lbp.sum() + 1e-6)
-        
-        # Combiner les features
-        feature = np.hstack([hist, hist_lbp])
-        
-        features.append(feature)
-    
-    return np.array(features)
-
-
 def flatten_images(images):
     """
     Exemple d'application:
@@ -97,16 +63,16 @@ def sift_features(images, max_descriptors=500):
 # ==================================================
 
 # # test
-# import dataset as d
+import dataset as d
+import matplotlib.pyplot as plt
 
-# X_test_sift = hog_features(d.X_train[:5])
+X_test_sift = sift_features(d.X_train[:5])
 
-# print(X_test_sift[:5])
+print(X_test_sift[:5])
 
 # ==================================================
 
 # Visualisation des points clés SIFT
-from matplotlib import pyplot as plt
 
 def visualize_sift_features(images, sift_features_list, max_descriptors=500):
     """
